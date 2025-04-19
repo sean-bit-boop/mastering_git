b5220a55d0836628cbe5eb127a82a2fc539668e2
Sean <vinluan0116@gmail.com>
2025-04-19 12:25:20 +0800

    Revert "adding the console log github"
    This reverts commit 64357339007a932437ba6849443a51618bd8f8c7.

--- hello.js
+++ hello.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 console.log("Hello, world!");
 console.log("Hello, GitHub Actions!");
 console.log("Hello from the dev-DummyTwo!");
 console.log("This is a bad code!");
+=======
+console.log("Hello, world!");
+>>>>>>> parent of 6435733 (adding the console log github)
